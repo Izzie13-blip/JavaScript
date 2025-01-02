528e9f03677fff4aee6b29fab896c5afe21b31f8
Izzie13-blip <marketbyamie@gmail.com>
2025-01-02 20:08:05 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('pipi1').innerHTML ="This is my first javascript program";
+document.getElementById('pipi2').innerHTML ="I am really excited today.";
+// variable code
+var num1 = 10;
+var num2 = 10;
+var sum = num1+num2
+document.getElementById('pipi3').innerHTML ="The sum of 2 numbers is :" + sum ;
