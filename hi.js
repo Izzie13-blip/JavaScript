3d5be155b3b23168e83c7d5068695419dfd9d6cc
Izzie13-blip <marketbyamie@gmail.com>
2025-01-09 21:35:30 +0100

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,19 @@
+var num1 = 10;
+var num2 = 10;
+var sum = num1+num2
+document.getElementById('pipi1').innerHTML ="The sum of 2 numbers is :" + sum ;
+ 
+var num3 = 15
+var num4 = 3
+var sub = num3-num4
+document.getElementById('pipi2').innerHTML ="The minus of 2 numbers is :" - minus ;
+
+var num4 = 10
+var num5 = 10
+var mul = num4*num5
+document.getElementById('pipi3').innerHTML ="The mul of 2 numbers is :" * mul ;
+
+var num6 = 15
+var num7 = 3
+var div = num6/num7
+document.getElementById('pipi4').innerHTML ="The div of 2 numbers is :" / div ;
